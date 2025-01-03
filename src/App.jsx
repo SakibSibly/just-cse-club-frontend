0a516fa0ebb460049c8b90e301054e0fe4065e09
SakibSibly <sibly@duck.com>
2025-01-03 23:18:09 +0600

    Route to admin Blog, Notices and Events maangement added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./assets/css/App.css"
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import MainComponent from './shared/MainComponent/MainComponent'
+import AdminDashboard from './pages/AdminDashboard/AdminDashboard'
 import Home from "./pages/Home/Home"
 import Login from "./pages/Login/Login"
 import Register from "./pages/Register/Register"
@@ -10,7 +11,15 @@ import Notices from './pages/Notices/Notices'
 import About from './pages/About/About'
 import Treasury from './pages/Treasury/Treasury'
 import NotFound from "./pages/NotFound/NotFound"
+import AdminRoute from './components/AdminRoute/AdminRoute'
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute"
+import BlogList from "./components/BlogList/BlogList"
+import BlogForm from "./components/BlogForm/BlogForm"
+import EventList from "./components/EventList/EventList"
+import EventForm from "./components/EventForm/EventForm"
+import NoticeList from "./components/NoticeList/NoticeList"
+import NoticeForm from "./components/NoticeForm/NoticeForm"
+
 
 const Logout = () => {
   localStorage.clear()
@@ -29,6 +38,16 @@ function App() {
      <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainComponent children={<Home />} />} />
+        <Route path="/admin" element={<MainComponent children={<AdminRoute><AdminDashboard /></AdminRoute>} />} />
+        <Route path="/admin/blogs" element={<MainComponent children={<AdminRoute><BlogList /></AdminRoute>} />} />
+        <Route path="/admin/blogs/new" element={<MainComponent children={<AdminRoute><BlogForm /></AdminRoute>} />} />
+        <Route path="/admin/blogs/edit/:id" element={<MainComponent children={<AdminRoute><BlogForm /></AdminRoute>} />} />
+        <Route path="/admin/events/" element={<MainComponent children={<AdminRoute><EventList /></AdminRoute>} />} />
+        <Route path="/admin/events/new" element={<MainComponent children={<AdminRoute><EventForm /></AdminRoute>} />} />
+        <Route path="/admin/events/edit/:id" element={<MainComponent children={<AdminRoute><EventForm /></AdminRoute>} />} />
+        <Route path="/admin/notices/" element={<MainComponent children={<AdminRoute><NoticeList /></AdminRoute>} />} />
+        <Route path="/admin/notices/new" element={<MainComponent children={<AdminRoute><NoticeForm /></AdminRoute>} />} />
+        <Route path="/admin/notices/edit/:id" element={<MainComponent children={<AdminRoute><NoticeForm /></AdminRoute>} />} />
         <Route path="/home" element={<MainComponent children={<Home />} />} />
         <Route path="/login" element={<MainComponent children={<Login />} />} />
         <Route path="/register" element={<MainComponent children={<RegisterAndLogout />} />} />
