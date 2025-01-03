1c822d48165ebcb5d5824774ae31078211b59096
SakibSibly <sibly@duck.com>
2025-01-03 08:09:53 +0600

    Route to blogs set in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import MainComponent from './shared/MainComponent/MainComponent'
 import Home from "./pages/Home/Home"
 import Login from "./pages/Login/Login"
 import Register from "./pages/Register/Register"
+import Blogs from './pages/Blogs/Blogs'
 import Events from './pages/Events/Events'
 import Notices from './pages/Notices/Notices'
 import About from './pages/About/About'
@@ -32,6 +33,8 @@ function App() {
         <Route path="/login" element={<MainComponent children={<Login />} />} />
         <Route path="/register" element={<MainComponent children={<RegisterAndLogout />} />} />
         <Route path="/logout" element={<Logout />} />
+        <Route path='/blogs' element={<MainComponent children={<Blogs />} />} />
+        <Route path='/blogs/:id' element={<MainComponent children={<Blogs />} />} />
         <Route path='/events' element={<MainComponent children={<Events />} />} />
         <Route path='/events/:id' element={<MainComponent children={<Events />} />} />
         <Route path='/notices' element={<MainComponent children={<Notices />} />} />
