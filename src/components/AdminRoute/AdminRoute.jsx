30044793a0974b29a999a4f133bdecc9b221c0f6
SakibSibly <sibly@duck.com>
2025-01-05 17:06:13 +0600

    Admin Route is handled with the api call from the server

--- src/components/AdminRoute/AdminRoute.jsx
+++ src/components/AdminRoute/AdminRoute.jsx
@@ -1,21 +1,18 @@
 import { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import api from "../../api";
-import { EMAIL } from "../../constants";
 
 const AdminRoute = ({ children }) => {
     const [admin, setAdmin] = useState(null);
-    const email = localStorage.getItem(EMAIL);
 
     useEffect(() => {
-        if (email) {
-            api.post("api/verify/admin/", { email })
-                .then(() => setAdmin(true))
-                .catch(() => setAdmin(false));
-        } else {
-            setAdmin(false);
+        const verifyAdmin = () => {
+            api.post("api/verify/admin/")
+            .then(() => setAdmin(true))
+            .catch(() => setAdmin(false));
         }
-    }, [email]);
+        verifyAdmin();
+    }, []);
     
     if (admin === null) {
         return <div>Loading...</div>;
