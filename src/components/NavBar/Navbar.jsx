b6b9634f01507652acd810ce2b6684fe4acd723e
SakibSibly <sibly@duck.com>
2025-01-05 19:45:50 +0600

    Active NavBar tab Indication implemented

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,5 +1,5 @@
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { ACCESS_TOKEN } from "../../constants";
 import api from "../../api";
@@ -8,6 +8,7 @@ import api from "../../api";
 const Navbar = () => {
     const [loggedIn, setLoggedIn] = useState(false);
     const [adminUser, setAdminUser] = useState(false);
+    const location = useLocation(); // Get current route
 
     useEffect(() => {
         if (localStorage.getItem(ACCESS_TOKEN)) {
@@ -20,62 +21,52 @@ const Navbar = () => {
                 setAdminUser(false);
                 console.error(error);
             });
-        }
+        };
         HandleAdminStatus();
-    }
-    , []);
+    }, []);
 
     const HandleUserStatus = () => {
         if (loggedIn) {
-            return (
-                <div>
-                    <Link to="/logout">Logout</Link>
-                </div>
-            )
+            return <Link to="/logout">Logout</Link>;
         } else {
-            return (
-                <div>
-                    <Link to="/login">Login</Link>
-                </div>
-            )
+            return <Link to="/login">Login</Link>;
         }
-    }
+    };
 
     return (
         <nav className="navbar">
+            <ul className="navbar-menu">
+                <li>
+                    <Link to="/home" className={location.pathname.startsWith("/home") || location.pathname === "/" ? "active-link" : ""}>Home</Link>
+                </li>
+                <li>
+                    <Link to="/blogs" className={location.pathname.startsWith("/blogs") ? "active-link" : ""}>Blogs</Link>
+                </li>
+                <li>
+                    <Link to="/events" className={location.pathname.startsWith("/events") ? "active-link" : ""}>Events</Link>
+                </li>
+                <li>
+                    <Link to="/notices" className={location.pathname.startsWith("/notices") ? "active-link" : ""}>Notices</Link>
+                </li>
+                <li>
+                    <Link to="/treasury" className={location.pathname.startsWith("/treasury") ? "active-link" : ""}>Treasury</Link>
+                </li>
+                <li>
+                    <Link to="/about" className={location.pathname.startsWith("/about") ? "active-link" : ""}>About</Link>
+                </li>
+            </ul>
+            <div>
                 <ul className="navbar-menu">
+                    {adminUser && (
+                        <li>
+                            <Link to="/admin" className={location.pathname.startsWith("/admin") ? "active-link" : ""}>Admin Dashboard</Link>
+                        </li>
+                    )}
                     <li>
-                        <Link to="/home" >Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/blogs">Blogs</Link>
-                    </li>
-                    <li>
-                        <Link to="/events">Events</Link>
-                    </li>
-                    <li>
-                        <Link to="/notices">Notices</Link>
-                    </li>
-                    <li>
-                        <Link to="/treasury">Treasury</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
+                        <HandleUserStatus />
                     </li>
                 </ul>
-                <div>
-                    <ul className="navbar-menu">
-                        {
-                            adminUser &&
-                            <li>
-                            <Link to="/admin">Admin Dashboard</Link>
-                            </li>
-                        }
-                        <li>
-                            <HandleUserStatus />
-                        </li>
-                    </ul>
-                </div>
+            </div>
         </nav>
     );
 };
