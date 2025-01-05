4ab5986840bb22b4f39f85a9d812092e9e88eef2
SakibSibly <sibly@duck.com>
2025-01-05 17:09:07 +0600

    404 Page Not Found page added

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,11 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import "./NotFound.css";
 
 const NotFound = () => {
-    return (
-        <div className="notfound">
-        <h1>Not Found</h1>
-        </div>
-    );
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h2>Oops! Page Not Found</h2>
+      <p>The page you are looking for might have been removed or doesn't exist.</p>
+      <Link to="/" className="home-link">
+        Go Back Home
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
