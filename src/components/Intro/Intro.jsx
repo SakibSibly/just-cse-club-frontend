22c382a9cbeabf433914c7b3c5e1549207224e30
SakibSibly <sibly@duck.com>
2025-01-15 00:07:30 +0600

    Intro is shown only once with the help of session storage

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,43 +1,40 @@
-import './Intro.css'
+import { useEffect, useState } from "react";
+import "./Intro.css";
 
 const Intro = () => {
+    const [showIntro, setShowIntro] = useState(() => {
+        return sessionStorage.getItem("introShown") !== "true"; // Check if already shown
+    });
+
+    useEffect(() => {
+        if (showIntro) {
+            sessionStorage.setItem("introShown", "true"); // Mark as shown for this session
+        }
+    }, [showIntro]);
+
+    if (!showIntro) return null; // Don't render if already shown in this tab
+
     const introText = "Welcome To The CSE Club Of JUST";
     const varsity = "Jashore University Of Science and Technology";
 
     return (
-      <>
         <div id="intro">
-            <div class="handshake">
-            {
-                introText.split(' ').map((word, index) => (
-                <span className="text" 
-                    key={index} 
-                    style={{
-                        animationDelay: `${index*0.25}s`,
-                        animationDuration: !index? '3s':'0s',
-                    }}>{word}</span>
-                ))
-            }
+            <div className="handshake">
+                {introText.split(" ").map((word, index) => (
+                    <span className="text" key={index} style={{ animationDelay: `${index * 0.25}s` }}>
+                        {word}
+                    </span>
+                ))}
             </div>
-            
-            <div class="varsity">
-            {
-                varsity.split('').map((char,index) => (
-                <span className="text"
-                    key={index}
-                    style={{
-                        animationDelay: `${index*0.025}s`,
-                    }}
-                >{char}</span>
-    
-                ))
-    
-            } 
+            <div className="varsity">
+                {varsity.split("").map((char, index) => (
+                    <span className="text" key={index} style={{ animationDelay: `${index * 0.025}s` }}>
+                        {char}
+                    </span>
+                ))}
             </div>
         </div>
-    
-      </>
     );
-}
+};
 
 export default Intro;
